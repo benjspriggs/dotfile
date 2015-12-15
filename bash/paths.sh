e2c18c6a0c047305d2d94f3b852c5815956fb921
Benjamin Spriggs <benjspriggs@sprico.com>
2015-12-14 16:26:02 -0800

    Update gitignore to ignore paths, move paths to paths.sh

---
+++ bash/paths.sh
@@ -0,0 +1,8 @@
+# paths used for system-dependent scripts
+# java_home
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+# git_home
+export GIT_HOME=/mnt/lo/benjspriggs/git
+
+export PATH=$HOME/bin:$PATH
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
