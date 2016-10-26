78b514ab5a3ab34aa85e80e5021c93f9ad131ab4
Benjamin Spriggs <benjspriggs@sprico.com>
2016-10-26 10:50:49 -0700

    Update install

--- install.sh
+++ install.sh
@@ -11,13 +11,20 @@ DIR=$(pwd)
 
 for f in ${all[@]}
 do
-  pre=$HOME/.
-  old_path=$pre$( basename $f)
-  rep_path=$DIR/$f
-  mv -n $old_path $old_path\.bak
-  ln -fns $rep_path $old_path 2>/dev/null
+	pre=$HOME/.
+	old_path=$pre$( basename $f)
+	rep_path=$DIR/$f
+	mv -n $old_path $old_path\.bak
+	ln -fns $rep_path $old_path 2>/dev/null
 done
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle &&
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install vim sline
+git clone https://github.com/jpalardy/vim-slime ~/.vim/bundle/vim-slime
+# install vim table mode
+git clone https://github.com/dhruvasagar/vim-table-mode ~/.vim/bundle/vim-table-mode
+# install ccimpl.vim
+git clone https://github.com/vim-scripts/ccimpl.vim ~/.vim/plugin/ccimpl
